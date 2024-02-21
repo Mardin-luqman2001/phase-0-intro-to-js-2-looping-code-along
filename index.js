a65d1b5f0e0ec66b62f6de31f274160b28d80acd
Mardin <mardinomer001@gmail.com>
2024-02-21 12:17:03 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+// Function to generate thank you messages for each name provided
+function writeCards(names, eventName) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return messages;
+  }
+  
+  // Function to count down from a provided number to zero
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
